2c387f7d1df50cd3355c0f44885820468ebd15e6
HJQ <>
2020-05-11 17:48:09 +0800

    提交

--- QLayout/QLayout/Class/UIView+QLayout.h
+++ QLayout/QLayout/Class/UIView+QLayout.h
@@ -27,54 +27,54 @@ NS_ASSUME_NONNULL_BEGIN
 /// top constant
 - (NSLayoutConstraint *) q_topEqualSuperView;
 - (NSLayoutConstraint *) q_topConstant: (CGFloat) constant;
-- (NSLayoutConstraint *) q_top: (NSLayoutYAxisAnchor *) superViewLayoutAnchor constant: (CGFloat) constant;
 - (NSLayoutConstraint *) q_topLessThanOrEqual: (CGFloat) constant;
 - (NSLayoutConstraint *) q_topGreaterThanOrEqual: (CGFloat) constant;
+- (NSLayoutConstraint *) q_top: (NSLayoutYAxisAnchor *) superViewLayoutAnchor constant: (CGFloat) constant;
 - (NSLayoutConstraint *) q_topLessThanOrEqual: (NSLayoutYAxisAnchor *) superViewLayoutAnchor constant: (CGFloat) constant;
 - (NSLayoutConstraint *) q_topGreaterThanOrEqual: (NSLayoutYAxisAnchor *) superViewLayoutAnchor constant: (CGFloat) constant;
 
 /// left
 - (NSLayoutConstraint *) q_leftEqualSuperView;
 - (NSLayoutConstraint *) q_leftConstant: (CGFloat) constant;
-- (NSLayoutConstraint *) q_left: (NSLayoutXAxisAnchor *) superViewLayoutAnchor constant: (CGFloat) constant;
 - (NSLayoutConstraint *) q_leftLessThanOrEqual: (CGFloat) constant;
 - (NSLayoutConstraint *) q_leftGreaterThanOrEqual: (CGFloat) constant;
+- (NSLayoutConstraint *) q_left: (NSLayoutXAxisAnchor *) superViewLayoutAnchor constant: (CGFloat) constant;
 - (NSLayoutConstraint *) q_leftLessThanOrEqual: (NSLayoutXAxisAnchor *) superViewLayoutAnchor constant: (CGFloat) constant;
 - (NSLayoutConstraint *) q_leftGreaterThanOrEqual: (NSLayoutXAxisAnchor *) superViewLayoutAnchor constant: (CGFloat) constant;
 
 /// leading constant
 - (NSLayoutConstraint *) q_leadingEqualSuperView;
 - (NSLayoutConstraint *) q_leadingConstant: (CGFloat) constant;
-- (NSLayoutConstraint *) q_leading: (NSLayoutXAxisAnchor *) superViewLayoutAnchor constant: (CGFloat) constant;
 - (NSLayoutConstraint *) q_leadingLessThanOrEqual: (CGFloat) constant;
 - (NSLayoutConstraint *) q_leadingGreaterThanOrEqual: (CGFloat) constant;
+- (NSLayoutConstraint *) q_leading: (NSLayoutXAxisAnchor *) superViewLayoutAnchor constant: (CGFloat) constant;
 - (NSLayoutConstraint *) q_leadingLessThanOrEqual: (NSLayoutXAxisAnchor *) superViewLayoutAnchor constant: (CGFloat) constant;
 - (NSLayoutConstraint *) q_leadingGreaterThanOrEqual: (NSLayoutXAxisAnchor *) superViewLayoutAnchor constant: (CGFloat) constant;
 
 /// bottom constant
 - (NSLayoutConstraint *) q_bottomEqualSuperView;
 - (NSLayoutConstraint *) q_bottomConstant: (CGFloat) constant;
-- (NSLayoutConstraint *) q_bottom: (NSLayoutYAxisAnchor *) superViewLayoutAnchor constant: (CGFloat) constant;
 - (NSLayoutConstraint *) q_bottomLessThanOrEqual: (CGFloat) constant;
 - (NSLayoutConstraint *) q_bottomGreaterThanOrEqual: (CGFloat) constant;
+- (NSLayoutConstraint *) q_bottom: (NSLayoutYAxisAnchor *) superViewLayoutAnchor constant: (CGFloat) constant;
 - (NSLayoutConstraint *) q_bottomLessThanOrEqual: (NSLayoutYAxisAnchor *) superViewLayoutAnchor constant: (CGFloat) constant;
 - (NSLayoutConstraint *) q_bottomGreaterThanOrEqual: (NSLayoutYAxisAnchor *) superViewLayoutAnchor constant: (CGFloat) constant;
 
 /// right constant
 - (NSLayoutConstraint *) q_rightEqualSuperView;
 - (NSLayoutConstraint *) q_rightConstant: (CGFloat) constant;
-- (NSLayoutConstraint *) q_right: (NSLayoutXAxisAnchor *) superViewLayoutAnchor constant: (CGFloat) constant;
 - (NSLayoutConstraint *) q_rightLessThanOrEqual: (CGFloat) constant;
 - (NSLayoutConstraint *) q_rightGreaterThanOrEqual: (CGFloat) constant;
+- (NSLayoutConstraint *) q_right: (NSLayoutXAxisAnchor *) superViewLayoutAnchor constant: (CGFloat) constant;
 - (NSLayoutConstraint *) q_rightLessThanOrEqual: (NSLayoutXAxisAnchor *) superViewLayoutAnchor constant: (CGFloat) constant;
 - (NSLayoutConstraint *) q_rightGreaterThanOrEqual: (NSLayoutXAxisAnchor *) superViewLayoutAnchor constant: (CGFloat) constant;
 
 /// trailing constant
 - (NSLayoutConstraint *) q_trailingEqualSuperView;
 - (NSLayoutConstraint *) q_trailingConstant: (CGFloat) constant;
-- (NSLayoutConstraint *) q_trailing: (NSLayoutXAxisAnchor *) superViewLayoutAnchor constant: (CGFloat) constant;
 - (NSLayoutConstraint *) q_trailingLessThanOrEqual: (CGFloat) constant;
 - (NSLayoutConstraint *) q_trailingGreaterThanOrEqual: (CGFloat) constant;
+- (NSLayoutConstraint *) q_trailing: (NSLayoutXAxisAnchor *) superViewLayoutAnchor constant: (CGFloat) constant;
 - (NSLayoutConstraint *) q_trailingLessThanOrEqual: (NSLayoutXAxisAnchor *) superViewLayoutAnchor constant: (CGFloat) constant;
 - (NSLayoutConstraint *) q_trailingGreaterThanOrEqual: (NSLayoutXAxisAnchor *) superViewLayoutAnchor constant: (CGFloat) constant;
 
